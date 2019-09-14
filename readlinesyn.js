5fce87a5297efde1be07f0d1848280093e4d5191
cocoon-yang <mistyang@126.com>
2019-09-14 09:51:55 +0800

    Update readlinesyn.js

--- readlinesyn.js
+++ readlinesyn.js
@@ -68,47 +68,53 @@ LineReader.prototype.close = function( )
 LineReader.prototype.next = function( ) 
 { 
         var self = this;
+ 
+        if(0 == self._fd)  
+        {  
+            console.log( 'next(): invalid file.');  
+             return;  
+        }  
+  
+        var _idxStart = 0;  
+        var idx;   
+        if ((idx = self._leftOver.indexOf("\n", _idxStart)) !== -1) {  
+            var line = self._leftOver.substring(_idxStart, idx);  
+  
+            _idxStart = idx + 1;  
+            self._leftOver = self._leftOver.substring(_idxStart);  
+     
+            return line;  
+          }  
 
-        if(0 == self._fd)
-        {
-             return;
-        }
-
-	var _idxStart = 0;
-        var idx = 0; 
-	while(( self._leftOver.indexOf('\n', _idxStart)) == -1){
-            var read;
-            try{
-                read = fs.readSync( self._fd, self._buffer, 0, self._bufferSize, null)
-            } 
-            catch ( exception )
-            {
-                 console.log( 'reading file failed.');
-                 self.close(); 
-                 return;
-            }
-            if (read !== 0) {
-              self._leftOver  += self._buffer.toString('utf8', 0, read);
-            }else {
-                 try{
-                     fs.closeSync(self._fd);
-                  }
-                 catch ( exception )
-                 {
-                    console.log( 'closing file failed.');
-                 }
-                 self._EOF = true;
-                 self._fd = 0; 
-                 return self._leftOver;
-            }
-	}
-       if ((idx = self._leftOver.indexOf('\n', _idxStart)) !== -1) {
-            var line = self._leftOver.substring(_idxStart, idx);
-            _idxStart = idx + 1;
-            self._leftOver = self._leftOver.substring(_idxStart);
-            _idxStart = 0;
-            return line;
-        } 
+        if ((idx = self._leftOver.indexOf("\n", _idxStart)) == -1) {    
+            var read;  
+  
+            try{  
+                read = fs.readSync( self._fd, self._buffer, 0, self._bufferSize, self._position); 
+            }   
+            catch ( exception )  
+            {  
+                 console.log( 'next(): reading file failed.');  
+                 self.close();   
+                 return;  
+            }  
+  
+            if (read !== 0) {  
+              self._leftOver  += self._buffer.toString('utf8', 0, read); 
+              return self.next(); 
+            } else {  
+                 try{  
+                     fs.closeSync(self._fd);  
+                  }  
+                 catch ( exception )  
+                 {  
+                    console.log( 'next(): closing file failed.');  
+                 }  
+                 self._EOF = true;  
+                 self._fd = 0;   
+                 return self._leftOver;  
+            }  
+         }  
 }
 
 LineReader.prototype.open =  function( thePath ) 
